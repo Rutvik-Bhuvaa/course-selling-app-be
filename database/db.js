fa452c2c05b6cf3543db0a609aa72d076e41a072
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-02-07 12:53:26 +0530

    Define schema in db

--- database/db.js
+++ database/db.js
@@ -1,13 +1,100 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 mongoose.connect("");
 
-const userSchema = new mongoose.Schema();
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
-const adminSchema = new mongoose.Schema();
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
-const courseSchema = new mongoose.Schema();
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  creatorId: {
+    type: ObjectId,
+    ref: "Admin",
+    required: true,
+    index: true,
+  },
+});
 
-const purchaseSchema = new mongoose.Schema();
+const purchaseSchema = new mongoose.Schema({
+  userId: {
+    type: ObjectId,
+    ref: "User",
+    required: true,
+    index: true,
+  },
+  courseId: {
+    type: ObjectId,
+    ref: "Course",
+    required: true,
+    index: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 const Admin = mongoose.model("Admin", adminSchema);
