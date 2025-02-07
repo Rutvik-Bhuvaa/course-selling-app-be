c23e2368536807cbc7150769686651a2307b14bd
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-02-07 12:32:08 +0530

    Added skeleton of db

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose.connect("");
+
+const userSchema = new mongoose.Schema();
+
+const adminSchema = new mongoose.Schema();
+
+const courseSchema = new mongoose.Schema();
+
+const purchaseSchema = new mongoose.Schema();
+
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+const Purchase = mongoose.model("Purchase", purchaseSchema);
